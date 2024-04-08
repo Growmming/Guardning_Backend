a66dafef9ba396e3707d0c131bf79a9b011b1bbb
Junyeong An <52746279+Junyeong-An@users.noreply.github.com>
2024-04-08 17:09:28 +0900

    Update SecurityConfig.java

--- src/main/java/com/growmming/gurdening/config/SecurityConfig.java
+++ src/main/java/com/growmming/gurdening/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
     public CorsConfigurationSource configurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOriginPatterns(List.of("http://localhost:5173", "https://gurdening.vercel.app"));
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:5173", "http://localhost", "https://gurdening.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Access-Control-Allow-Credentials", "Authorization", "Set-Cookie"));
